f624f9c9e1e31c7ee3aafc10c7a7404140fd9bd3
Ravi Upadhyay <raviu@raviu.fareast.corp.microsoft.com>
2015-09-27 23:39:27 +0530

    Adding only unique cards on deck

--- public/javascripts/set.js
+++ public/javascripts/set.js
@@ -2,23 +2,46 @@ var set = angular.module('set', []);
 
 set.controller('deckController',
 function($scope){
-	$scope.cards = [];
-	$scope.getNumber = function(num){
-		return new Array(num);
-	}
+
 	var Card = function(iconColors, bgColors, shapes){
-		return {
-			iconColor: iconColors[Math.floor(Math.random() * 3)],
-			bgColor: bgColors[Math.floor(Math.random() * 3)],
-			icon: shapes[Math.floor(Math.random() * 3)],
-			count: Math.floor(Math.random() * 3) + 1
+		var iconColors = ['red', 'green', 'blue'];
+		var bgColors = ['white', 'yellow', 'pink'];
+		var shapes = ['bolt', 'child', 'heartbeat'];
+		this.iconColor = iconColors[Math.floor(Math.random() * 3)];
+		this.bgColor = bgColors[Math.floor(Math.random() * 3)];
+		this.icon = shapes[Math.floor(Math.random() * 3)];
+		this.count = Math.floor(Math.random() * 3) + 1;
+		this.equals = function (other){
+			if( this.iconColor == other.iconColor &&
+				this.bgColors == other.bgColors &&
+				this.icon == other.icon &&
+				this.count == other.count
+			){
+				return true;
+			}
+			else{
+				return false;
+			}
 		};
 	};
-	for(var i = 0; i < 12; i++){
-		$scope.cards.push(new Card(
-			['red', 'green', 'blue'], 
-			['white', 'yellow', 'pink'], 
-			['bolt', 'child', 'heartbeat']
-		));
+
+	$scope.cards = [];
+	for(var i = 1; i < 12; i++){
+		var foundNew = false
+		while(!foundNew){
+			var temp = new Card();
+			if(i == 0) 
+				foundNew = true;
+			for(var j = 0; j < i; j++){
+				if(temp.equals($scope.cards[j]))
+					break;
+				if(j == i - 1)
+					foundNew = true;
+			}
+		}
 	}
+
+	$scope.getNumber = function(num){
+	return new Array(num);
+}
 });
